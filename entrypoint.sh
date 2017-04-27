d31687d935db5741b680d3bfba34895af55087e2
Vitaliy Tverdokhlib <vitaliytv@nitra.ua>
2017-04-27 09:53:50 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,12 @@
+
 #!/bin/sh
 # docker entrypoint script
 
-openssl req \
-  -x509 \
-  -nodes \
-  -days "$DAYS" \
-  -newkey rsa:2048 \ 
-  -keyout "$ISSUER_NAME.key" \ 
-  -out "$ISSUER_NAME.crt" \
-  -subj "/C=$COUNTY/ST=$STATE/L=$LOCATION/O=$ORGANISATION/CN=$ISSUER_CN"
+openssl req -x509 -nodes -days "$DAYS" -newkey rsa:2048 -keyout "$ISSUER_NAME.key" -out "$ISSUER_NAME.crt" -subj "/C=$COUNTY/ST=$STATE/L=$LOCATION/O=$ORGANISATION/CN=$ISSUER_CN"
 
-  # copy certificate to volume
-  cp "$ISSUER_NAME.crt" "$CERT_DIR"
-  cp "$ISSUER_NAME.key" "$CERT_DIR"  
+# copy certificate to volume
+cp "$ISSUER_NAME.crt" "$CERT_DIR"
+cp "$ISSUER_NAME.key" "$CERT_DIR"  
 
 # run command passed to docker run
 exec "$@"
